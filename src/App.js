2ea9ad98681707234f2cebfb61635cfde81d5580
Akash Kumar <89804806+akashqumar@users.noreply.github.com>
2023-04-27 02:02:16 +0530

    imagecropapp

---
+++ src/App.js
@@ -0,0 +1,194 @@
+import React, { useState, useRef, useEffect } from "react";
+
+import ReactCrop, { centerCrop, makeAspectCrop } from "react-image-crop";
+import { canvasPreview } from "./canvasPreview";
+
+import "react-image-crop/dist/ReactCrop.css";
+import "./App.css";
+import { Button, TextField } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+// This is to demonstate how to make and center a % aspect crop
+// which is a bit trickier so we use some helper functions.
+function centerAspectCrop(mediaWidth, mediaHeight, aspect) {
+  return centerCrop(
+    makeAspectCrop(
+      {
+        unit: "%",
+        width: 90,
+      },
+      aspect,
+      mediaWidth,
+      mediaHeight
+    ),
+    mediaWidth,
+    mediaHeight
+  );
+}
+
+export default function App() {
+  const [imgSrc, setImgSrc] = useState("");
+  const previewCanvasRef = useRef(null);
+  const imgRef = useRef(null);
+  const hiddenAnchorRef = useRef(null);
+  const blobUrlRef = useRef("");
+  const [crop, setCrop] = useState();
+  const [completedCrop, setCompletedCrop] = useState();
+  const [scale, setScale] = useState(1);
+  const [rotate, setRotate] = useState(0);
+  const [aspect, setAspect] = useState(16 / 9);
+
+  function onSelectFile(e) {
+    if (e.target.files && e.target.files.length > 0) {
+      setCrop(undefined); // Makes crop preview update between images.
+      const reader = new FileReader();
+      reader.addEventListener("load", () =>
+        setImgSrc(reader.result?.toString() || "")
+      );
+      reader.readAsDataURL(e.target.files[0]);
+    }
+  }
+
+  function onImageLoad(e) {
+    if (aspect) {
+      const { width, height } = e.currentTarget;
+      setCrop(centerAspectCrop(width, height, aspect));
+    }
+  }
+
+  function onDownloadCropClick() {
+    if (!previewCanvasRef.current) {
+      throw new Error("Crop canvas does not exist");
+    }
+
+    previewCanvasRef.current.toBlob((blob) => {
+      if (!blob) {
+        throw new Error("Failed to create blob");
+      }
+      if (blobUrlRef.current) {
+        URL.revokeObjectURL(blobUrlRef.current);
+      }
+      blobUrlRef.current = URL.createObjectURL(blob);
+      console.log(hiddenAnchorRef.current);
+      if (hiddenAnchorRef.current) {
+        hiddenAnchorRef.current.href = blobUrlRef.current;
+        hiddenAnchorRef.current.click();
+      }
+    });
+  }
+
+  useEffect(() => {
+    const updateCanvasPreview = async () => {
+      if (
+        completedCrop?.width &&
+        completedCrop?.height &&
+        imgRef.current &&
+        previewCanvasRef.current
+      ) {
+        canvasPreview(
+          imgRef.current,
+          previewCanvasRef.current,
+          completedCrop,
+          scale,
+          rotate
+        );
+      }
+    };
+  
+    updateCanvasPreview();
+  }, [completedCrop, scale, rotate]);
+  
+
+  function handleToggleAspectClick() {
+    if (aspect) {
+      setAspect(undefined);
+    } else if (imgRef.current) {
+      const { width, height } = imgRef.current;
+      setAspect(16 / 9);
+      setCrop(centerAspectCrop(width, height, 16 / 9));
+    }
+  }
+
+  return (
+    <div className="box" style={{marginTop:"150px"}}>
+    <h1>IMAGE CROPPER</h1>
+      <div className="Crop-Controls">
+        <input  type="file" accept="image/*" onChange={onSelectFile} />
+        <div>
+          <label htmlFor="scale-input">Scale: </label>
+          <TextField
+          variant="outlined"
+            id="scale-input"
+            type="number"
+            step="0.1"
+            value={scale}
+            disabled={!imgSrc}
+            onChange={(e) => setScale(Number(e.target.value))}
+          />
+        </div>
+        <div>
+          <label htmlFor="rotate-input">Rotate: </label>
+          <TextField
+          variant="outlined"
+            id="rotate-input"
+            type="number"
+            value={rotate}
+            disabled={!imgSrc}
+            onChange={(e) =>
+              setRotate(Math.min(180, Math.max(-180, Number(e.target.value))))
+            }
+          />
+        </div>
+        <div>
+          <Button onClick={handleToggleAspectClick} color="primary" variant="contained">Toggle aspect {aspect ? "off" : "on"}</Button>
+        </div>
+      </div>
+      {!!imgSrc && (
+        <ReactCrop
+          crop={crop}
+          onChange={(_, percentCrop) => setCrop(percentCrop)}
+          onComplete={(c) => setCompletedCrop(c)}
+          aspect={aspect}
+        >
+          <img
+            ref={imgRef}
+            alt="Crop me"
+            src={imgSrc}
+            style={{ transform: `scale(${scale}) rotate(${rotate}deg)` }}
+            onLoad={onImageLoad}
+          />
+        </ReactCrop>
+      )}
+      {!!completedCrop && (
+        <>
+          <div>
+            <canvas
+              ref={previewCanvasRef}
+              style={{
+                border: "1px solid black",
+                objectFit: "contain",
+                width: completedCrop.width,
+                height: completedCrop.height,
+              }}
+            />
+          </div>
+          <div>
+            <Button onClick={onDownloadCropClick} color="primary" variant="contained">Download Crop</Button>
+            <a
+              ref={hiddenAnchorRef}
+              href={blobUrlRef.current}
+              download
+              style={{
+                position: "absolute",
+                top: "-200vh",
+                visibility: "hidden",
+              }}
+            >
+              Hidden download
+            </a>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
